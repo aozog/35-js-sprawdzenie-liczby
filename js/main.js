984e25c11de7962c4eb048319e6c0d0e9e607dbb
aozog <artur.ozog@gmail.com>
2020-08-26 13:46:59 +0200

    poprawiony zapis

--- js/main.js
+++ js/main.js
@@ -1,19 +1,12 @@
-var one = "Uwielbiam JavaScript";
-var two = "Jestem świetnym programistą";
-
-var num1 = one.length;
-var num2 = two.length;
-
-
-function strings(one, two) {
-    if (num1 > num2) {
-        return one;
-    } else if (num1 < num2) {
-        return two;
+function przedzial(n) {
+    if (n>=100 && n<=200) {
+        return "Liczba znajduje się w przedziale";
+    } else {
+        return "Liczba nie znajduje się w przedziale";
     };
 };
 
 
 
-console.log(strings(one, two));
+console.log(przedzial(n));
 
